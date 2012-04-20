5e2d2252ac53abe473b4efd8fa3d8102163dbf89
Abhi Yerra <abhi.yerra@mylookout.com>
2012-04-20 14:17:47 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hannibal::Application.config.secret_token = 'cfbc2bd8ff22f08c62c8d1d33820adfcfe45aa9c9bebe1d93e7037589e9c17fe5f88eccc92dd5fc572fafd801de35c38900c32dd8b1178fb6be3e2af03804396'
