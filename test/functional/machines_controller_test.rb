009306caa15ab72a63f9d0312f248e432f14fb5a
Abhi Yerra <abhi@berkeley.edu>
2012-05-25 14:05:07 -0700

    Added machine controller

--- test/functional/machines_controller_test.rb
+++ test/functional/machines_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PortsControllerTest < ActionController::TestCase
+class MachinesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
